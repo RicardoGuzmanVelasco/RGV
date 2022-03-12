cfe7cec1f2050dd460ae49c083a9d3e35324d197
RGV <ricardo.guzman.velasco@outlook.com>
2022-03-12 09:03:48 +0100

    test(roman): add roman symbol

--- Assets/RGV.Math/Runtime/Roman/RomanNumeral.cs
+++ Assets/RGV.Math/Runtime/Roman/RomanNumeral.cs
@@ -7,19 +7,21 @@ namespace RGV.Math.Runtime.Roman
 {
     public record RomanNumeral
     {
-        readonly string symbols;
+        readonly string chars;
+
+        IList<RomanSymbol> Symbols => chars.Select(c => new RomanSymbol(c)).ToList();
 
         #region Constructors
         public RomanNumeral() : this("I") { }
 
         public RomanNumeral(string symbols)
         {
-            if(symbols.Contains('j'))
+            if(!symbols.All(RomanSymbol.IsSymbol))
                 throw new ArgumentOutOfRangeException(nameof(symbols));
             if(IsAdditiveNotation(symbols))
                 throw new NotSupportedException("Additive notation is not supported");
 
-            this.symbols = symbols;
+            chars = symbols;
         }
 
         public RomanNumeral(int number)
@@ -29,6 +31,7 @@ namespace RGV.Math.Runtime.Roman
         }
         #endregion
 
+        #region Conversions
         public static implicit operator RomanNumeral(string symbols)
         {
             return new RomanNumeral(symbols);
@@ -41,40 +44,81 @@ namespace RGV.Math.Runtime.Roman
 
         public override string ToString()
         {
-            return symbols;
+            return chars;
         }
+        
+        int ToInt()
+        {
+            if(chars.Length == 1)
+                return Symbols.Single();
+
+            int total = Symbols.Last();
+            for(var i = chars.Length - 2; i >= 0; i--)
+            {
+                int addend = Symbols[i];
+                if(NextSymbolIsLower(i))
+                    addend *= -1;
+                total += addend;
+            }
+
+            return total;
+
+            bool NextSymbolIsLower(int index)
+            {
+                return Symbols[index + 1] > Symbols[index];
+            }
+        }
+        #endregion
 
         static bool IsAdditiveNotation(string symbols)
         {
             return symbols.LongestContiguous() >= 4;
         }
 
-        int ToInt()
+        #region Nested
+        record RomanSymbol : IComparable<RomanSymbol>
         {
-            if(symbols.Length == 1)
-                return Symbols[symbols.Single()];
-            
-            var total = new RomanNumeral(""+symbols.Last()).ToInt();
-            for(var i = symbols.Length - 2; i >= 0; i--)
+            public static readonly Dictionary<char, int> Values = new Dictionary<char, int>
             {
-                var addend = new RomanNumeral("" + symbols[i]).ToInt();
-                if(Symbols[symbols[i + 1]] > Symbols[symbols[i]])
-                    addend *= -1;
-                total += addend;
+                ['I'] = 1,
+                ['V'] = 5,
+                ['X'] = 10,
+                ['L'] = 50,
+                ['C'] = 100,
+                ['D'] = 500,
+                ['M'] = 1000
+            };
+            
+            readonly char symbol;
+
+            public RomanSymbol(char source)
+            {
+                if(!IsSymbol(source))
+                    throw new ArgumentOutOfRangeException();
+                
+                symbol = source;
             }
 
-            return total;
+            public static bool IsSymbol(char c)
+            {
+                return Values.ContainsKey(c);
+            }
+
+            public static implicit operator int(RomanSymbol r)
+            {
+                return r.ToInt();
+            }
+
+            int ToInt()
+            {
+                return Values[symbol];
+            }
+
+            public int CompareTo(RomanSymbol other)
+            {
+                return ToInt().CompareTo(other.ToInt());
+            }
         }
-        
-        static readonly Dictionary<char, int> Symbols = new Dictionary<char, int>
-        {
-            ['I'] = 1,
-            ['V'] = 5,
-            ['X'] = 10,
-            ['L'] = 50,
-            ['C'] = 100,
-            ['D'] = 500,
-            ['M'] = 1000,
-        };
+        #endregion
     }
 }
