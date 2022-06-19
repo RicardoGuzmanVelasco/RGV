80557d00158935dbdd250eaca8ce0b9763eb3aac
RGV <ricardo.guzman.velasco@outlook.com>
2022-06-19 15:03:33 +0200

    fix(designbycontract): add assembly properties to expose internals to tests

---
+++ Assets/RGV.DesignByContract/Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("RGV")]
+[assembly: AssemblyProduct("DesignByContract")]
+[assembly: AssemblyTitle("RGV.DesignByContract")]
+
+[assembly: AssemblyVersion("0.1.0")]
+
+[assembly: InternalsVisibleTo("RGV.DesignByContract.Tests")]
