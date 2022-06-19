3c24038cff5aea210076a2ee197a323da242efa4
RGV <ricardo.guzman.velasco@outlook.com>
2022-06-19 17:58:14 +0200

    build(designbycontract): upgrade package version to 0.2.0

--- Assets/RGV.DesignByContract/Runtime/Properties/AssemblyInfo.cs
+++ Assets/RGV.DesignByContract/Runtime/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("DesignByContract")]
 [assembly: AssemblyTitle("RGV.DesignByContract")]
 
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
 
 [assembly: InternalsVisibleTo("RGV.DesignByContract.Tests")]
