eca54899d2c6a6792bcfb6e6a2dd14961dcc90f4
RGV <ricardo.guzman.velasco@outlook.com>
2022-06-26 13:32:37 +0200

    feat(math): add large numbers with not only single suffixes

--- Assets/RGV.Math/Runtime/LargeNumbers/Numbig.Suffix.cs
+++ Assets/RGV.Math/Runtime/LargeNumbers/Numbig.Suffix.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RGV.Math.Runtime.LargeNumbers
 {
@@ -15,8 +16,35 @@ namespace RGV.Math.Runtime.LargeNumbers
 
         static string SuffixAfter(string suffix)
         {
+            suffix ??= "";
+            return !NextSuffixIsSingle(suffix)
+                ? CompoundSuffixAfter(suffix)
+                : SingleSuffixAfter(suffix);
+        }
+
+        static string SingleSuffixAfter(string suffix)
+        {
             var nextSuffixIndex = SingleSuffixes.IndexOf(suffix ?? "") + 1;
             return SingleSuffixes[nextSuffixIndex];
+        }
+
+        static bool NextSuffixIsSingle(string suffix)
+        {
+            return SingleSuffixes.Contains(suffix) &&
+                   SingleSuffixes.IndexOf(suffix) < SingleSuffixes.Count - 1;
+        }
+
+        static string CompoundSuffixAfter(string suffix)
+        {
+            if(suffix == SingleSuffixes.Last())
+                return "aa";
+
+            if(suffix.All(c => c == 'z'))
+                return string.Concat(Enumerable.Repeat("a", suffix.Length + 1));
+
+            var suffixChars = suffix.ToCharArray();
+            suffixChars[^1]++;
+            return new string(suffixChars);
         }
     }
 }
--- Assets/RGV.Math/Tests/LargeNumbersTests.cs
+++ Assets/RGV.Math/Tests/LargeNumbersTests.cs
@@ -14,7 +14,7 @@ namespace RGV.Math.Tests
         }
 
         [Test]
-        public void NextSuffix()
+        public void Next()
         {
             new Numbig(1, "k").Should().Be(new Numbig(1000));
             new Numbig(8, "M").Should().Be(new Numbig(8000, "k"));
@@ -22,12 +22,34 @@ namespace RGV.Math.Tests
         }
 
         [Test]
-        public void NotContiguousSuffix()
+        public void NotContiguous()
         {
             new Numbig(8, "M").Should().Be(new Numbig(8000000));
         }
 
+        [Test]
+        public void FromSingleToNotSingle()
+        {
+            new Numbig(1040, "T").Should().Be(new Numbig(1.04f, "aa"));
+            new Numbig(1000000, "B").Should().Be(new Numbig(1, "aa"));
+        }
+
+        [Test]
+        public void FromNotSingleToNotSingleWithSameLength()
+        {
+            new Numbig(1000, "aa").Should().Be(new Numbig(1, "ab"));
+            new Numbig(1000, "zf").Should().Be(new Numbig(1, "zg"));
+        }
+
+        [Test]
+        public void FromNotSingleToNotSingle_WithDifferentLength()
+        {
+            new Numbig(1000, "zz").Should().Be(new Numbig(1, "aaa"));
+            new Numbig(1000, "zzz").Should().Be(new Numbig(1, "aaaa"));
+        }
+
         //ignore case
         //non negative?
+        //addition
     }
 }
